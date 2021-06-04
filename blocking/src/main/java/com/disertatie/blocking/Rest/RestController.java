b7df3dde9dac771ac6ac9afc4cd3b44cfdac1b81
luxxxoor <vrincean.alex@gmail.com>
2021-06-04 20:59:59 +0300

    YAS

--- blocking/src/main/java/com/disertatie/blocking/Rest/RestController.java
+++ blocking/src/main/java/com/disertatie/blocking/Rest/RestController.java
@@ -1,11 +1,16 @@
 package com.disertatie.blocking.Rest;
 
 import com.disertatie.blocking.algorithms.Algorithms;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.client.RestTemplate;
 
+import java.math.BigDecimal;
 import java.math.BigInteger;
 import java.util.concurrent.CompletableFuture;
 
@@ -54,5 +59,28 @@ public class RestController {
                     System.out.println("[test3] Finished: " + c);
                     return result;
                 });
+    }
+
+    @GetMapping("/test4")
+    public CompletableFuture<ResponseEntity<BigInteger>> test4(@RequestParam Integer m, @RequestParam Integer n, @RequestParam String ip) {
+        System.out.println("[test4] Started: " + counter);
+        int c = counter;
+        counter += 1;
+
+        return CompletableFuture.supplyAsync(() -> Algorithms.ack(m,n))
+                .thenApply((result) -> callSecondServer(result, ip))
+                .thenApply((response) -> new ResponseEntity<>(response.join(), OK))
+                .thenApply((result) -> {
+                    System.out.println("[test4] Finished: " + c);
+                    return result;
+                });
+    }
+
+    @Async
+    private CompletableFuture<BigInteger> callSecondServer(BigInteger ackermannResponse, String ip) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://" + ip + ":8080/test3/?m=" + ackermannResponse.mod(BigInteger.valueOf(3)) +
+                "&n=" + ackermannResponse.mod(BigInteger.valueOf(10));
+        return CompletableFuture.completedFuture(restTemplate.getForObject(url, BigInteger.class));
     }
 }
