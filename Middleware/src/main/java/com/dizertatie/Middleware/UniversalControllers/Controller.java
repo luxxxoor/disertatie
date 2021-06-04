5cfbc2a8b0919ae395551f16ebc3da55b60a20ce
luxxxoor <vrincean.alex@gmail.com>
2021-06-05 01:16:55 +0300

    getMapping

--- Middleware/src/main/java/com/dizertatie/Middleware/UniversalControllers/Controller.java
+++ Middleware/src/main/java/com/dizertatie/Middleware/UniversalControllers/Controller.java
@@ -4,6 +4,7 @@ import com.dizertatie.Middleware.Tools.FluidIO;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @FluidIO
@@ -12,7 +13,7 @@ class Controller
     @Autowired
     private Environment env;
 
-    @RequestMapping("/test")
+    @GetMapping("/test")
     String test() {
         return env.getProperty("middleware.type");
     }
