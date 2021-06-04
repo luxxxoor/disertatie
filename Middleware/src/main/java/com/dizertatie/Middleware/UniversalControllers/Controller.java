2af776b5db490fd9a4af5936881f89cd6fc7f233
bobby <vilie0215@gmail.com>
2021-06-05 01:02:49 +0300

    scheleton for middleware tool

---
+++ Middleware/src/main/java/com/dizertatie/Middleware/UniversalControllers/Controller.java
@@ -0,0 +1,19 @@
+package com.dizertatie.Middleware.UniversalControllers;
+
+import com.dizertatie.Middleware.Tools.FluidIO;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@FluidIO
+class Controller
+{
+    @Autowired
+    private Environment env;
+
+    @RequestMapping("/test")
+    String test() {
+        return env.getProperty("middleware.type");
+    }
+}
