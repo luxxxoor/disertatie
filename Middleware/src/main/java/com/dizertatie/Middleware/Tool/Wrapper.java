e0764073aa99005634ca3cb39316093972b0fcee
luxxxoor <vrincean.alex@gmail.com>
2021-06-04 22:08:54 +0300

    wip wrapper

--- Middleware/src/main/java/com/dizertatie/Middleware/Tool/Wrapper.java
+++ Middleware/src/main/java/com/dizertatie/Middleware/Tool/Wrapper.java
@@ -3,15 +3,14 @@ package com.dizertatie.Middleware.Tool;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public class Wrapper<T, U> {
-    T obj;
+public class Wrapper<T> {
+    private final T obj;
 
     Wrapper(T obj){
         this.obj = obj;
     }
 
-    Wrapper<U> get() {
-        Mono<Integer> i;
-        Flux<Integer> i1;
+    T get() {
+        return obj;
     }
 }
