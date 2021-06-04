ffc5477129f379434d1b2a10484bd8bfdda8687a
bobby <vilie0215@gmail.com>
2021-06-04 21:01:14 +0300

    Something

--- Middleware/src/main/java/com/dizertatie/Middleware/MiddlewareApplication.java
+++ Middleware/src/main/java/com/dizertatie/Middleware/MiddlewareApplication.java
@@ -1,5 +1,6 @@
 package com.dizertatie.Middleware;
 
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
@@ -8,6 +9,7 @@ public class MiddlewareApplication {
 
 	public static void main(String[] args) {
         SpringApplicationBuilder builder = new SpringApplicationBuilder(MiddlewareApplication.class);
+        builder.web(WebApplicationType.REACTIVE);
 
         builder.run(args);
 	}
