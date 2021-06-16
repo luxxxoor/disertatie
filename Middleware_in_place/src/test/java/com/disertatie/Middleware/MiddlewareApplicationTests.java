9a94b26b9323a400b15c75d8f88b8a85209ab20e
bobby <vilie0215@gmail.com>
2021-06-16 14:11:07 +0300

    In place sometest

--- Middleware_in_place/src/test/java/com/disertatie/Middleware/MiddlewareApplicationTests.java
+++ Middleware_in_place/src/test/java/com/disertatie/Middleware/MiddlewareApplicationTests.java
@@ -1,16 +1,20 @@
 package com.disertatie.Middleware;
 
 import com.disertatie.Middleware.Tools.FluidIO;
+import com.disertatie.Middleware.Tools.MovingRecord;
+import com.disertatie.Middleware.Tools.RequestType;
+
+import org.javatuples.Pair;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import reactor.core.publisher.Mono;
 
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.function.Supplier;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @SpringBootTest
 class MiddlewareApplicationTests {
@@ -97,4 +101,34 @@ class MiddlewareApplicationTests {
 
 		assert blockingCounter.get() > nonblockingCounter.get();
 	}
+
+    @Test
+    void testMovingRecordRandom() {
+        int tries = 1000;
+        Random random = new Random();
+
+        MovingRecord m = new MovingRecord(10, 16);
+        List<Pair<Instant, RequestType>> recordings = new ArrayList<>();
+        List<RequestType> accumulatedTypes = new ArrayList<>();
+
+        for (int i=0; i<tries; ++i) {
+            boolean newRecording = random.nextBoolean();
+
+            if (recordings.isEmpty() || newRecording) {
+                var recording = m.startRecording();
+                accumulatedTypes.add(recording.getValue1());
+                recordings.add(recording);
+            } else {
+                var randIdx = random.nextInt(recordings.size());
+                var recording = recordings.get(randIdx);
+                recordings.remove(recording);
+
+                m.endRecording(recording);
+            }
+        }
+        
+        assert ! accumulatedTypes.isEmpty();
+        assert accumulatedTypes.size() == 7;
+        accumulatedTypes.forEach(System.out::println);
+    }
 }
